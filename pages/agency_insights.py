4839447e3eb1b12845d8d3bbd9dbad93d7dc751b
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 12:32:03 +0530

    Update agency_insights.py

--- pages/agency_insights.py
+++ pages/agency_insights.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from utils.data_loader import load_data
+from utils.charts import agency_chart
 
+df = load_data()
+
+st.title("Agency Insights")
+
+st.plotly_chart(
+    agency_chart(df),
+    use_container_width=True
+)
