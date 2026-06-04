3ce1cef183174e0dfa8a5fcc4fd4e4772d395b0e
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 12:33:03 +0530

    Update predictive_analytics.py

--- pages/predictive_analytics.py
+++ pages/predictive_analytics.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.title("Predictive Analytics")
+
+st.info(
+"""
+Future Version:
+
+✔ Cost Prediction
+✔ Success Prediction
+✔ Launch Risk Analysis
+✔ Machine Learning Models
+"""
+)
