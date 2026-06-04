9655c38e085da1a55f5dfd3182c71847ef903ee4
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 12:29:48 +0530

    Update insights.py

--- utils/insights.py
+++ utils/insights.py
@@ -1,1 +1,17 @@
+def generate_insights(df):
 
+    insights = []
+
+    insights.append(
+        f"Total Missions: {len(df)}"
+    )
+
+    insights.append(
+        f"Total Agencies: {df['Agency'].nunique()}"
+    )
+
+    insights.append(
+        f"Countries: {df['Country_Region'].nunique()}"
+    )
+
+    return insights
