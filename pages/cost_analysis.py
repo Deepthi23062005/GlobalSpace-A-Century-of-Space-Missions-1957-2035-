c81072d29ad69add37aa9910ae3d629c21c6ce4c
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 12:32:37 +0530

    Update cost_analysis.py

--- pages/cost_analysis.py
+++ pages/cost_analysis.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import plotly.express as px
+from utils.data_loader import load_data
 
+df = load_data()
+
+st.title("Cost Analysis")
+
+fig = px.histogram(
+    df,
+    x="Cost_USD_Million",
+    nbins=30
+)
+
+st.plotly_chart(fig, use_container_width=True)
