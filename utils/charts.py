65a35742ea311944e4a1f14d82ea96271e1a503b
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 12:29:02 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,1 +1,28 @@
+import plotly.express as px
 
+def mission_status_chart(df):
+    fig = px.pie(
+        df,
+        names="Status",
+        title="Mission Status Distribution"
+    )
+    return fig
+
+
+def agency_chart(df):
+    agency = (
+        df.groupby("Agency")
+        .size()
+        .reset_index(name="Missions")
+        .sort_values("Missions", ascending=False)
+        .head(10)
+    )
+
+    fig = px.bar(
+        agency,
+        x="Agency",
+        y="Missions",
+        title="Top 10 Agencies"
+    )
+
+    return fig
