786959c9366eb14112ec1364313f7680343ce72b
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 11:58:41 +0530

    Update execute_dashboard.py

--- pages/execute_dashboard.py
+++ pages/execute_dashboard.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv("data/Space_Missions_Dataset.csv")
+
+st.title("Executive Dashboard")
+
+col1,col2,col3 = st.columns(3)
+
+col1.metric("Total Missions",len(df))
+col2.metric("Agencies",df["Agency"].nunique())
+col3.metric("Countries",df["Country_Region"].nunique())
+
+fig = px.pie(
+    df,
+    names="Status",
+    title="Mission Status"
+)
+
+st.plotly_chart(fig,use_container_width=True)
