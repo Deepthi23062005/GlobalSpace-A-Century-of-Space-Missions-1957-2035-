7b3e5dc12de3a5ea9e5558e91677c236e197bc52
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 12:31:22 +0530

    Update mission_analysis.py

--- pages/mission_analysis.py
+++ pages/mission_analysis.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import plotly.express as px
+from utils.data_loader import load_data
 
+df = load_data()
+
+st.title("Mission Analysis")
+
+mission_year = (
+    df.groupby("Launch_Year")
+    .size()
+    .reset_index(name="Missions")
+)
+
+fig = px.line(
+    mission_year,
+    x="Launch_Year",
+    y="Missions",
+    markers=True
+)
+
+st.plotly_chart(fig, use_container_width=True)
